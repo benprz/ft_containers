f0235d71e9c7350476a913028c491869cb1ae3ca
Benjamin Perez <bperez@z3r12p3.42lyon.fr>
2022-06-16 18:59:50 +0200

    .

--- inc/map_tree.hpp
+++ inc/map_tree.hpp
@@ -26,22 +26,41 @@ class map_tree
 		~map_tree(){}
 		void insert_node(key_type key, mapped_type value)
 		{
-			node* new_node = _alloc(1);
-			new_node->pair.first = key;
-			new_node->pair.second = value;
+			node* n = _alloc(1);
+			n->pair.first = key;
+			n->pair.second = value;
 			if (!root)
-				root = new_node;
+				root = n;
 			else
 			{
 				node* current_node = root;
 				while (current_node->left_child || current_node->right_child)
 				{
 					if (key < current_node->pair.first)
-						current_node = current_node->left_child
+						current_node = current_node->left_child;
+					else
+						current_node = current_node->right_child;
+
 				}
+				if (key < current_node->pair.first)
+					current_node->left_child = n;
+				else
+					current_node->right_child = n;
 			}
 		}
-		void search_node();
+		node* search_node(key_type key)
+		{
+			node* current_node = root;
+			while (current_node->left_child || current_node->right_child)
+			{
+				if (key == current_node->pair.first)
+					return current_node;
+				else if (key < current_node->pair.first)
+					current_node = current_node->left_child;
+				else
+					current_node = current_node->right_child;
+			}
+		};
 		void delete_node();
 };
 
